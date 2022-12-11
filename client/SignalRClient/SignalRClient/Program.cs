142b54603726eb1f8ed648272ee6d9e5d6680e39
Vinícius <vinigsouza.bnu@gmail.com>
2022-12-11 18:02:32 -0300

    feat: implementa client

--- client/SignalRClient/SignalRClient/Program.cs
+++ client/SignalRClient/SignalRClient/Program.cs
@@ -1,1 +1,14 @@
-﻿const string url = "https://localhost:7299/chat";
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+const string url = "https://localhost:7299/chat";
+
+ await using var connection = new HubConnectionBuilder()
+    .WithUrl(url)
+    .Build();
+
+await connection.StartAsync();
+
+await foreach (var date in connection.StreamAsync<DateTime>("Streaming"))
+{
+    Console.WriteLine(date);
+}
